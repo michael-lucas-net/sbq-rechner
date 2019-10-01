f00847b8d8ea074256b7298eac875fd35d53a035
Michael Lucas <michiqt@live.de>
2019-10-01 16:49:55 +0200

    Fixes

--- src/components/QuotenRechner.vue
+++ src/components/QuotenRechner.vue
@@ -1,112 +1,80 @@
 <template>
-    <div>
-        <div class="form-calculate">
-            <img
-                    class="logo mb-4"
-                    src="../assets/rechner.png"
-                    alt="Rechner Logo">
+	<div>
+		<div class="form-calculate">
 
-            <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Mitarbeiter</span>
-                </div>
-                <input type="number" min="1" class="form-control" v-model="mitarbeiter" @change="changeBeruecksichtigt"
-                       placeholder="Mitarbeiter" required="required">
-            </div>
+			<Logo/>
 
-            <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Auszubildene</span>
-                </div>
-                <input type="number" min="0" class="form-control" v-model="azubis" @change="changeBeruecksichtigt"
-                       placeholder="Mitarbeiter" required="required">
-            </div>
-            <Ergebnis :result="result"/>
+			<div class="input-group input-group-sm mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text">Mitarbeiter</span>
+				</div>
+				<input type        = "number"
+				       min         = "1"
+				       class       = "form-control"
+				       v-model     = "mitarbeiter"
+				       @change     = "changeBeruecksichtigt"
+				       placeholder = "Mitarbeiter"
+				       required    = "required">
+			</div>
 
-            <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Geringfügige Mitarbeiter</span>
-                </div>
-                <input type="number" min="0" class="form-control" v-model="geringis" @change="changeBeruecksichtigt"
-                       placeholder="Mitarbeiter" required="required">
-            </div>
+			<div class="input-group input-group-sm mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text">Auszubildene</span>
+				</div>
+				<input type        = "number"
+				       min         = "0"
+				       class       = "form-control"
+				       v-model     = "azubis"
+				       @change     = "changeBeruecksichtigt"
+				       placeholder = "Mitarbeiter"
+				       required    = "required">
+			</div>
 
-            <div class="input-group input-group-sm mb-3">
-                <div class="input-group-prepend">
-                    <span class="input-group-text" id="inputGroup-sizing-sm">Benachteiligte Mitarbeiter</span>
-                </div>
-                <input type="number" min="0" class="form-control" v-model="benachteiligte" @change="changeBeruecksichtigt"
-                       placeholder="Mitarbeiter" required="required">
-            </div>
+			<div class="input-group input-group-sm mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text">Geringfügige Mitarbeiter</span>
+				</div>
+				<input
+						type        = "number"
+						min         = "0"
+						class       = "form-control"
+						v-model     = "geringis"
+						@change     = "changeBeruecksichtigt"
+						placeholder = "Mitarbeiter"
+						required    = "required">
+			</div>
 
-            <div class="fieldContainer">
-                <div
-                        class="alert"
-                        v-bind:class="{ 'alert-success': ergebnis > 0, 'alert-secondary': ergebnis == 0}"
-                        role="alert">
-                        <small>
-                        Anteil benachteiligter Mitarbeiter: 
-                        <span>{{ ergebnis }}%</span>
-                    </small>
-                </div>
-            </div>
+			<div class="input-group input-group-sm mb-3">
+				<div class="input-group-prepend">
+					<span class="input-group-text">Benachteiligte Mitarbeiter</span>
+				</div>
+				<input
+						type        = "number"
+						min         = "0"
+						class       = "form-control"
+						v-model     = "benachteiligte"
+						@change     = "changeBeruecksichtigt"
+						placeholder = "Mitarbeiter"
+						required    = "required">
+			</div>
 
-            <Bottom />     
-            <Bottom/>
-        </div>
-    </div>
+			<Ergebnis :result="result"/>
+			<Bottom/>
+
+		</div>
+	</div>
 </template>
 
 <script>
-import Bottom from './bottom';
-    export default {
-        name: "Quotenrechner",
-        components: {
-            Bottom
-        },
-        data() {
-            return {
-                mitarbeiter: 1,
-                azubis: 0,
-                geringis: 0,
-                benachteiligte: 0,
-                beruecksichtigt: 0,
-                ergebnis: 0
-            };
-        },
-        methods: {
-            /**
-             * Rundet auf Zweikommastellen
-             */
-            round(number) {
-                return Number(number.toFixed(2));
-            },
-
-            /**
-             * Berechnet wieviele Mitarbeiter benachteiligt sind
-             */
-            calculate() {
-                this.ergebnis = this.beruecksichtigt > 0 && this.benachteiligte > 0
-                    ? this.round(this.benachteiligte / this.beruecksichtigt * 100)
-                    : 0;
-            },
-
-            /**
-             * Berücksichtigt werden alle Mitarbeiter abzüglich Azubis und geringfügige Mitarbeitern
-             */
-            changeBeruecksichtigt() {
-                this.beruecksichtigt = Number(this.mitarbeiter) - (Number(this.azubis) + Number(this.geringis));
-                this.calculate();
-            }
-        }
-    };
+import Logo from './logo';
 	import Bottom from './bottom';
 	import Ergebnis from './ergebnis';
+
 	export default {
 		name: "Quotenrechner",
 		components: {
+            Logo,
 			Bottom,
-			Field,
 			Ergebnis
 		},
 		data() {
@@ -125,8 +93,7 @@ import Bottom from './bottom';
 			 */
 			round(number) {
 				return Number(number.toFixed(2));
-			}
-			,
+			},
 
 			/**
 			 * Berechnet wieviele Mitarbeiter benachteiligt sind
@@ -150,51 +117,52 @@ import Bottom from './bottom';
 
 <style>
 
-    /* Allgemein */
-    body,
-    html {
-        height: 100%;
-    }
+	/* Allgemein */
+	body,
+	html {
+		height: 100%;
+	}
 
-    body {
-        display: -ms-flexbox;
-        display: -webkit-box;
-        display: flex;
-        -ms-flex-align: center;
-        -ms-flex-pack: center;
-        -webkit-box-align: center;
-        align-items: center;
-        -webkit-box-pack: center;
-        justify-content: center;
-        padding-top: 40px;
-        padding-bottom: 40px;
-        background-color: #f5f5f5;
-    }
+	body {
+		display: -ms-flexbox;
+		display: -webkit-box;
+		display: flex;
+		-ms-flex-align: center;
+		-ms-flex-pack: center;
+		-webkit-box-align: center;
+		align-items: center;
+		-webkit-box-pack: center;
+		justify-content: center;
+		padding-top: 40px;
+		padding-bottom: 40px;
+		background-color: #f5f5f5;
+	}
 
-    .logo {
-        width: 50px;
-    }
+	.fieldContainer {
+		margin: 10px 0;
+	}
 
-    .fieldContainer {
-        margin: 10px 0;
-    }
+	/* Form */
+	.form-calculate,
+	.input-group-text {
+		width: 100%;
+	}
 
-    /* Form */
-    .form-calculate {
-        width: 100%;
-    }
+	.form-calculate {
+		max-width: 330px;
+		padding: 15px;
+		margin: 0 auto;
+	}
 
-    .form-calculate {
-        max-width: 330px;
-        padding: 15px;
-        margin: 0 auto;
-    }
+	.form-calculate {
+		position: relative;
+		box-sizing: border-box;
+		height: auto;
+		padding: 10px;
+		font-size: 16px;
+	}
 
-    .form-calculate {
-        position: relative;
-        box-sizing: border-box;
-        height: auto;
-        padding: 10px;
-        font-size: 16px;
-    }
+	.input-group-prepend {
+		width: 60%;
+	}
 </style>
--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -4,7 +4,12 @@
         <Heart/>
         <section class="bottom">
             <p class="mt-3 mb-3 text-muted small">
-                &copy; 2019 Michi für Chrischi
+                &copy; 2019 
+                <a
+                    href="https://github.com/michael-lucas-net/" 
+                    target="_blank">
+                    Michi
+                </a> für Chrischi
             </p>
         </section>
     </div>
@@ -21,3 +26,10 @@
 		}
 	};
 </script>
+
+<style scoped>
+    a {
+        font-weight: bold;
+        color: #333;
+    }
+</style>
