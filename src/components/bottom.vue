648a9806d64a3de2a00cd36266f4d9358b9bf5e2
Michael Lucas <michael-lucas-net@users.noreply.github.com>
2019-11-14 16:29:39 +0100

    Aufraeumen (#4)
    * serve mit open
    
    * copyright ->bottom
    
    * Logo und Herz Komponenten entfernt
    
    * Update vue.config.js

--- src/components/bottom.vue
+++ src/components/bottom.vue
@@ -1,6 +1,8 @@
 <template lang="html">
   <div>
-    <Heart />
+    <div class="herz mt-5 animated infinite pulse">
+      <img src="../assets/herz.png" alt="Herz" class="herzImg" />
+    </div>
     <section class="bottom">
       <p class="mt-3 mb-3 text-muted small">
         &copy; 2019
@@ -13,20 +15,17 @@
   </div>
 </template>
 
-<script lang="js">
-import Heart from './herz';
-
-export default {
-    name: 'copyright',
-    components: {
-        Heart
-    }
-};
-</script>
-
 <style scoped>
 a {
   font-weight: bold;
   color: #333;
+}
+.herz {
+  font-size: 35px;
+  display: inline-block;
+}
+
+.herzImg {
+  width: 35px;
 }
 </style>
